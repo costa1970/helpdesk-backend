c4b73d488dcb28aa83942564993de8118a232efc
costa1970 <sergiocosta1970@gmail.com>
2021-08-29 12:37:38 -0300

    FindAll para Chamado

--- src/main/java/com/costa/helpdesk/services/ChamadoService.java
+++ src/main/java/com/costa/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.costa.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,15 @@ public class ChamadoService {
 	@Autowired
 	private ChamadoRepository repository;
 	
+	//==================================================================================================
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
 	}
+
+	//==================================================================================================
+	public List<Chamado> findAll() {
+		return repository.findAll();
+	}
+	//==================================================================================================
 }
